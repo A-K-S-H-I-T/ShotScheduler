3e0be4964b616ee959a1f7af6c7bfba46ef973f5
A-K-S-H-I-T <this.akshit.11@gmail.com>
2025-05-27 22:38:04 +0530

    started project ShotScheduler(Vaccine booking system)

---
+++ src/main/java/com/example/ShotScheduler/Enum/DayOfWeek.java
@@ -0,0 +1,11 @@
+package com.example.ShotScheduler.Enum;
+
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/main/java/com/example/ShotScheduler/ShotSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShotScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShotSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShotSchedulerApplication.class, args);
+	}
+
+}
